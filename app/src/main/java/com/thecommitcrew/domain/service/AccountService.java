3d8c4c7ff811ca2761096c0d77996339da4e015d
Louise Deeth <louisedeeth@gmail.com>
2026-09-21 15:51:24 +0000

    feat: start AccountService implementation with method stubs

---
+++ app/src/main/java/com/thecommitcrew/domain/service/AccountService.java
@@ -0,0 +1,34 @@
+package com.thecommitcrew.domain.service;
+
+import java.util.*;
+import com.thecommitcrew.domain.model.Order;
+import org.springframework.stereotype.Service;
+import com.thecommitcrew.domain.model.Account;
+import com.thecommitcrew.domain.model.Position;
+import com.thecommitcrew.domain.model.Money;
+
+@Service 
+public class AccountService {
+
+    public Account getAccount(Long accountId) {
+        //TO DO
+        return null;
+
+    }
+
+    public List<Position> getPositions(Long accountId) {
+        //TO DO
+        return null;
+    }
+
+    public List<Order> getOrders(Long accountId) {
+        //TO DO
+        return null;
+    }
+
+    public Money getBalance(Long accountId) {
+        //TO DO
+        return null;
+    }
+    
+}
