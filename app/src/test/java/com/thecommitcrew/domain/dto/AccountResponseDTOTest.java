b72b76d08965492ef2c1812826a51243b27b69ff
Myroslava Bunciuc <mira.bunciuc@gmail.com>
2026-09-23 15:27:09 +0000

    chore: rebase

--- app/src/test/java/com/thecommitcrew/domain/dto/AccountResponseDTOTest.java
+++ app/src/test/java/com/thecommitcrew/domain/dto/AccountResponseDTOTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigDecimal;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.thecommitcrew.domain.enums.AccountStatus;
