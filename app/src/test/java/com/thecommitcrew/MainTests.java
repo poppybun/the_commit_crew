2ffc789f65847aea7d21529f3e79f674e012cb57
Ian Gill <ian123gill@gmail.com>
2026-09-23 14:43:21 +0000

    feat: added Spring test dependencies and mock repository beans

--- app/src/test/java/com/thecommitcrew/MainTests.java
+++ app/src/test/java/com/thecommitcrew/MainTests.java
@@ -1,10 +1,27 @@
 package com.thecommitcrew;
 
+import com.thecommitcrew.persistence.repository.AccountRepository;
+import com.thecommitcrew.persistence.repository.InstrumentRepository;
+import com.thecommitcrew.persistence.repository.OrderRepository;
+import com.thecommitcrew.persistence.repository.PositionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class MainTests {
+
+  @MockitoBean
+  private AccountRepository accountRepository;
+
+  @MockitoBean
+  private PositionRepository positionRepository;
+
+  @MockitoBean
+  private OrderRepository orderRepository;
+
+  @MockitoBean
+  private InstrumentRepository instrumentRepository;
 
   @Test
   void contextLoads() {
