be36cdd7c3b5ca6737286be3ab00d29a76332c53
Myroslava Bunciuc <mira.bunciuc@gmail.com>
2026-09-25 13:24:31 +0000

    fix: made health endpoint available to all

--- auth/src/main/java/com/thecommitcrew/auth/config/SecurityConfig.java
+++ auth/src/main/java/com/thecommitcrew/auth/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
             authConfig
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(authEntryPointJwt))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/actuator/health").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
